835d435d2a435c5da77dde5be52ee7c88f70e20f
Alexander Levero <Alexander.Levero@gmail.com>
2023-04-20 22:30:38 +0300

    Feature 14 complete

--- receipts/forms.py
+++ receipts/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from receipts.models import Receipt, ExpenseCategory
+from receipts.models import Receipt, ExpenseCategory, Account
 
 
 class CreateForm(ModelForm):
@@ -19,3 +19,12 @@ class CreateCategoryForm(ModelForm):
     class Meta:
         model = ExpenseCategory
         fields = ["name"]
+
+
+class CreateAccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = [
+            "name",
+            "number",
+        ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from receipts.models import ExpenseCategory, Account, Receipt
 from django.contrib.auth.decorators import login_required
-from receipts.forms import CreateForm, CreateCategoryForm
+from receipts.forms import CreateForm, CreateCategoryForm, CreateAccountForm
 
 
 # Create your views here.
@@ -48,6 +48,24 @@ def create_category(request):
         "form": form,
     }
     return render(request, "receipts/create_category.html", context)
+
+
+@login_required
+def create_account(request):
+    if request.method == "POST":
+        form = CreateAccountForm(request.POST)
+        if form.is_valid():
+            receipts = form.save(commit=False)
+            receipts.owner = request.user
+            receipts.save()
+            return redirect("account_list")
+    else:
+        form = CreateAccountForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "receipts/create_account.html", context)
 
 
 @login_required
